e8933a07633370c0a0ac9f746b3e152712ee15fe
vijay <vijaysingh.gujar@radixweb.com>
2022-07-08 15:15:38 +0530

    minor changes in index.js

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
 require("dotenv").config()
 
-// const app = require("./routes/routes");
-
-// const port = process.env.API_PORT || 9000;
-
-// // server listening 
-// app.get('/', (req, res)=> {
-//   res.send('Welcome to upskill node api app')
-// })
-// app.listen(port, () => {
-//   console.log(`Server running on port ${port}`);
-// });
-
 const app = require("./routes/routes");
 
 const { API_PORT } = process.env;
-const port = API_PORT || 9000;
+const port = process.env.PORT || API_PORT;
 
+app.get('/', (req, res)=> {
+  res.send('Welcome to upskill node api app')
+})
 // server listening 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
